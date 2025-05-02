72382977b15d4aa2a83e093467d5babb1855781d
Tatiana <tatisalexa16@gmail.com>
2025-05-02 08:00:32 -0500

    Creación de información en el modelo usuario.java

--- miproyecto/src/main/java/com/miproyecto/modelo/Usuario.java
+++ miproyecto/src/main/java/com/miproyecto/modelo/Usuario.java
@@ -9,5 +9,43 @@ package com.miproyecto.modelo;
  * @author Personal
  */
 public class Usuario {
+    private String nombre;
+    private String apellido;
+    private String email;
+    private String password;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    
     
 }
